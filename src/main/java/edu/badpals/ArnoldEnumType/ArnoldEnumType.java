da971bc5b574384a9b9867411914e1aff7cd9a2c
Sergio Prieto García <serprigar@gmail.com>
2024-04-09 12:34:17 +0200

    Initial commit

---
+++ src/main/java/edu/badpals/ArnoldEnumType/ArnoldEnumType.java
@@ -0,0 +1,13 @@
+package edu.badpals.ArnoldEnumType;
+
+/**
+ * Hello world!
+ *
+ */
+public class ArnoldEnumType
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
